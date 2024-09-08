9ad967faa4c02390de2431c405cc135c2c59d94d
Renato Medeiros <renato.medeiros@pagtrue.onmicrosoft.com>
2024-09-08 14:25:08 -0300

    ceated crud

--- Controllers/LibrariesController.cs
+++ Controllers/LibrariesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using my_library_cosmos_db.Models;
 using my_library_cosmos_db.Services;
+using MyBooks.Core.Models;
 
 namespace my_library_cosmos_db.Controllers
 {
@@ -24,8 +25,30 @@ namespace my_library_cosmos_db.Controllers
             return Ok(response);
         }
 
+        [HttpPut]
+        public async Task<IActionResult> UpdateBook([FromBody] UpdateBookRequestModel model, Guid bookId)
+        {
+            var response = await _librariesService.UpdateBook(model, bookId);
+
+            return Ok(response);
+        }
 
 
+        [HttpGet("id")]
+        public async Task<IActionResult> GetBookById([FromQuery] Guid bookId, [FromQuery] string partitionKey)
+        {
+            var response = await _librariesService.GetBookById(bookId, partitionKey);
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBookList()
+        {
+            var response = await _librariesService.GetBookList();
+
+            return Ok(response);
+        }
 
     }
 }
