b2c2c92045eb99ee084ffef2f2014d58c72be16c
Renato Medeiros <renato.medeiros@pagtrue.onmicrosoft.com>
2024-09-12 17:07:42 -0300

    created new logic for send error for db context

--- Middlewares/ErrorMiddleware.cs
+++ Middlewares/ErrorMiddleware.cs
@@ -1,24 +1,28 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json;
 using System.Linq.Expressions;
 using System.Net;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace my_library_cosmos_db.Middlewares
 {
     public class ErrorMiddleware
     {
-        private readonly RequestDelegate next;
+        private readonly RequestDelegate _next;
+        private readonly Container _container; // Contêiner do Cosmos DB
 
-        public ErrorMiddleware(RequestDelegate next)
+        public ErrorMiddleware(RequestDelegate next, Container container)
         {
-            this.next = next;
+            _next = next;
+            _container = container; // Injetar o contêiner do Cosmos DB
         }
 
         public async Task Invoke(HttpContext context)
         {
             try
             {
-                await next(context);
+                await _next(context);
             }
             catch (Exception ex)
             {
@@ -26,25 +30,58 @@ namespace my_library_cosmos_db.Middlewares
             }
         }
 
-        private Task HandleExceptionAsync(HttpContext context, Exception ex)
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex)
         {
-            //gravar log de erro com traceId
-
-
             ErrorMiddlewareResponse errorMiddlewareResponse;
 
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT") == "Development")
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
             {
                 errorMiddlewareResponse = new ErrorMiddlewareResponse(HttpStatusCode.InternalServerError.ToString(), $"{ex.Message} {ex?.InnerException?.Message}");
             }
             else
             {
-                errorMiddlewareResponse = new ErrorMiddlewareResponse(HttpStatusCode.InternalServerError.ToString(), "An internal server error has ocurred");
+                errorMiddlewareResponse = new ErrorMiddlewareResponse(HttpStatusCode.InternalServerError.ToString(), "An internal server error has occurred");
             }
+
+            // Gravar log de erro no Cosmos DB usando o TraceId existente
+            var logEntry = new ErrorLogModel
+            {
+                Id = Guid.NewGuid().ToString(),
+                TraceId = errorMiddlewareResponse.TraceId.ToString(), // Reutilizando o TraceId
+                ErrorMessage = ex.Message,
+                StackTrace = ex.StackTrace,
+                Errors = "errors",
+                InnerException = ex.InnerException?.Message,
+                Timestamp = DateTime.UtcNow
+            };
+
+            var partitionKey = new PartitionKey(logEntry.Errors);
+
+            await _container.CreateItemAsync(logEntry, partitionKey); // Salvar o log no Cosmos DB
 
             var result = JsonConvert.SerializeObject(errorMiddlewareResponse);
             context.Response.ContentType = "application/json";
-            return context.Response.WriteAsync(result);
+            await context.Response.WriteAsync(result);
         }
     }
+
+    // Modelo para o log de erro
+    public class ErrorLogModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        public string Errors { get; set; }
+
+        public string TraceId { get; set; } // Reutilizando o TraceId da resposta
+
+        public string ErrorMessage { get; set; }
+
+        public string StackTrace { get; set; }
+
+        public string InnerException { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+
 }
